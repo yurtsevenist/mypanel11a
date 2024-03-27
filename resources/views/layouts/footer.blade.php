bdafcaa42ef3967dc17ab04ef84fc5d1ca532f88
yurtsevenist <yurtsevenist@hotmail.com>
2024-03-27 11:21:20 +0300

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,43 @@
+ <!-- Control Sidebar -->
+ <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.2.0
+    </div>
+  </footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+<!-- jQuery -->
+<script src="{{asset('tema')}}/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="{{asset('tema')}}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="{{asset('tema')}}/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('tema')}}/dist/js/adminlte.js"></script>
+
+<!-- PAGE PLUGINS -->
+<!-- jQuery Mapael -->
+<script src="{{asset('tema')}}/plugins/jquery-mousewheel/jquery.mousewheel.js"></script>
+<script src="{{asset('tema')}}/plugins/raphael/raphael.min.js"></script>
+<script src="{{asset('tema')}}/plugins/jquery-mapael/jquery.mapael.min.js"></script>
+<script src="{{asset('tema')}}/plugins/jquery-mapael/maps/usa_states.min.js"></script>
+<!-- ChartJS -->
+<script src="{{asset('tema')}}/plugins/chart.js/Chart.min.js"></script>
+
+<!-- AdminLTE for demo purposes -->
+<script src="{{asset('tema')}}/dist/js/demo.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="{{asset('tema')}}/dist/js/pages/dashboard2.js"></script>
+</body>
+</html>
+
